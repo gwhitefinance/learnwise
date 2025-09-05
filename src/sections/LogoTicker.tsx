b061f3e6fd98691e24aae17bd1083feef401f994
Chris White <comresusa@gmail.com>
2025-09-05 21:22:44 +0000

    okay use logos

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -8,42 +8,42 @@ import Image from "next/image";
 const universities = [
     {
         name: "Harvard University",
-        src: "https://picsum.photos/150/50?random=1",
-        hint: "university building"
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Harvard_shield.svg/1200px-Harvard_shield.svg.png",
+        hint: "university shield"
     },
     {
         name: "Stanford University",
-        src: "https://picsum.photos/150/50?random=2",
-        hint: "campus view"
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Stanford_Cardinal_logo.svg/1200px-Stanford_Cardinal_logo.svg.png",
+        hint: "tree logo"
     },
     {
         name: "MIT",
-        src: "https://picsum.photos/150/50?random=3",
-        hint: "modern architecture"
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/MIT_logo.svg/1200px-MIT_logo.svg.png",
+        hint: "modern logo"
     },
     {
         name: "University of Oxford",
-        src: "https://picsum.photos/150/50?random=4",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Oxford-University-Circlet.svg/1200px-Oxford-University-Circlet.svg.png",
         hint: "historic university"
     },
     {
         name: "University of Cambridge",
-        src: "https://picsum.photos/150/50?random=5",
+        src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Cambridge_University_shield.svg/1200px-Cambridge_University_shield.svg.png",
         hint: "college crest"
     },
     {
         name: "Caltech",
-        src: "https://picsum.photos/150/50?random=6",
-        hint: "science lab"
+        src: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/California_Institute_of_Technology_seal.svg/1200px-California_Institute_of_Technology_seal.svg.png",
+        hint: "science seal"
     },
     {
         name: "ETH Zurich",
-        src: "https://picsum.photos/150/50?random=7",
+        src: "https://upload.wikimedia.org/wikipedia/en/thumb/4/49/ETH_Zurich_logo.svg/1200px-ETH_Zurich_logo.svg.png",
         hint: "swiss university"
     },
     {
         name: "UCL",
-        src: "https://picsum.photos/150/50?random=8",
+        src: "https://upload.wikimedia.org/wikipedia/en/thumb/0/01/University_College_London_logo.svg/1200px-University_College_London_logo.svg.png",
         hint: "london university"
     },
 ];
@@ -79,7 +79,7 @@ export default function LogoTicker() {
                                           alt={`${uni.name} logo`}
                                           width={150}
                                           height={50}
-                                          className="object-contain"
+                                          className="object-contain h-12 w-auto"
                                           data-ai-hint={uni.hint}
                                         />
                                     </div>
