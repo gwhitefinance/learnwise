b23b4905220c0c86a07f13afd49ebde15ceb560f
Chris White <comresusa@gmail.com>
2025-09-05 20:06:41 +0000

    Try fixing this error: `Build Error: Export Avatar doesn't exist in targ

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,20 +1,51 @@
-import React, { HTMLAttributes } from "react";
-import { twMerge } from "tailwind-merge";
 
-const Avatar = (props: HTMLAttributes<HTMLDivElement>) => {
-    const { className, children, ...rest } = props;
+"use client"
 
-    return (
-        <div
-            className={twMerge(
-                "size-20 rounded-full overflow-hidden border-4 border-blue-500 p-1 bg-neutral-900",
-                className
-            )}
-            {...rest}
-        >
-            {children}
-        </div>
-    );
-};
+import * as React from "react"
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
-export default Avatar;
+import { cn } from "@/lib/utils"
+
+const Avatar = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      className
+    )}
+    {...props}
+  />
+))
+Avatar.displayName = AvatarPrimitive.Root.displayName
+
+const AvatarImage = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn("aspect-square h-full w-full", className)}
+    {...props}
+  />
+))
+AvatarImage.displayName = AvatarPrimitive.Image.displayName
+
+const AvatarFallback = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      className
+    )}
+    {...props}
+  />
+))
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+
+export { Avatar, AvatarImage, AvatarFallback }
