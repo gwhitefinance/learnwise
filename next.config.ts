357bfb99fd634245eec0522c14aa66bcb983b2f2
Chris White <comresusa@gmail.com>
2025-09-05 20:46:49 +0000

    why are the images not rendering and i cant see them?

--- next.config.ts
+++ next.config.ts
@@ -9,26 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    // remotePatterns are no longer needed as we are using local images.
   },
   devIndicators: {
     allowedDevOrigins: [
