372513d82c36ef400327e373513c66f649985805
Chris White <comresusa@gmail.com>
2025-09-05 21:22:10 +0000

    okay nevermind text sucks find small working images that work for each u

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -3,16 +3,49 @@
 
 import { motion } from "framer-motion";
 import React from "react";
+import Image from "next/image";
 
-const universityNames = [
-    "Harvard University",
-    "Stanford University",
-    "MIT",
-    "University of Oxford",
-    "University of Cambridge",
-    "Caltech",
-    "ETH Zurich",
-    "UCL",
+const universities = [
+    {
+        name: "Harvard University",
+        src: "https://picsum.photos/150/50?random=1",
+        hint: "university building"
+    },
+    {
+        name: "Stanford University",
+        src: "https://picsum.photos/150/50?random=2",
+        hint: "campus view"
+    },
+    {
+        name: "MIT",
+        src: "https://picsum.photos/150/50?random=3",
+        hint: "modern architecture"
+    },
+    {
+        name: "University of Oxford",
+        src: "https://picsum.photos/150/50?random=4",
+        hint: "historic university"
+    },
+    {
+        name: "University of Cambridge",
+        src: "https://picsum.photos/150/50?random=5",
+        hint: "college crest"
+    },
+    {
+        name: "Caltech",
+        src: "https://picsum.photos/150/50?random=6",
+        hint: "science lab"
+    },
+    {
+        name: "ETH Zurich",
+        src: "https://picsum.photos/150/50?random=7",
+        hint: "swiss university"
+    },
+    {
+        name: "UCL",
+        src: "https://picsum.photos/150/50?random=8",
+        hint: "london university"
+    },
 ];
 
 export default function LogoTicker() {
@@ -36,12 +69,19 @@ export default function LogoTicker() {
                     >
                         {Array.from({ length: 2 }).map((_, i) => (
                             <React.Fragment key={i}>
-                                {universityNames.map((name) => (
+                                {universities.map((uni) => (
                                     <div
-                                        key={name}
-                                        className="text-2xl font-medium text-white/80 whitespace-nowrap"
+                                        key={uni.name}
+                                        className="flex-shrink-0"
                                     >
-                                        {name}
+                                        <Image
+                                          src={uni.src}
+                                          alt={`${uni.name} logo`}
+                                          width={150}
+                                          height={50}
+                                          className="object-contain"
+                                          data-ai-hint={uni.hint}
+                                        />
                                     </div>
                                 ))}
                             </React.Fragment>
