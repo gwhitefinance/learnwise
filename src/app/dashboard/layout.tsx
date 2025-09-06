70fb39e05bf771a7c6b6f4b627114c85cf6626f7
Chris White <comresusa@gmail.com>
2025-09-06 16:03:17 +0000

    Try fixing this error: `Console Error: Encountered two children with the

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ import { Upload, Menu } from 'lucide-react';
 const DashboardIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1m0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1m10-17v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1m0 15h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1"/></svg>;
 const CoursesIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M20 5.88V18c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h12c.34 0 .67.09 1 .24V5.88l-2 .88V4H6v14h12V7.5l2-1.62zM12 12.26 6 10v-1.37l6 2.63l6-2.63V10l-6 2.26M13.83 6.43l-6 2.62v-1.5l6-2.62z"/></svg>;
 const VisualToolsIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M7 17.013H5V15h2zM7 13H5V11h2zM7 9H5V7h2zm4-2H9V5h2zm0 4H9V9h2zm0 4H9V13h2zm0 4H9V17h2zM19 9h-2V7h2zm-4 4h-2V9h2zm4-4h-2V5h2zm-4 8h-2V13h2zm4 4h-2V17h2zm0-4h-2V13h2z"/></svg>;
-const CommunityIcon = () => <svg xmlns="http://www.w3org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3s1.34 3 3 3m-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5S5 6.34 5 8s1.34 3 3 3m0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5m8 0c-.29 0-.62.02-.97.05c1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5"/></svg>;
+const CommunityIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3s1.34 3 3 3m-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5S5 6.34 5 8s1.34 3 3 3m0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5m8 0c-.29 0-.62.02-.97.05c1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5"/></svg>;
 const SettingsIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5s3.5 1.57 3.5 3.5s-1.57 3.5-3.5 3.5"/></svg>;
 const UploadIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M9 16h6v-6h4l-7-7l-7 7h4zM5 18h14v2H5z"/></svg>;
 
@@ -23,8 +23,8 @@ const UploadIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" heig
 const navItems = [
     { href: '/dashboard', icon: <DashboardIcon />, label: 'Dashboard' },
     { href: '/dashboard/courses', icon: <CoursesIcon />, label: 'My Courses' },
-    { href: '#', icon: <VisualToolsIcon />, label: 'Visual Tools' },
-    { href: '#', icon: <CommunityIcon />, label: 'Community' },
+    { href: '/dashboard/visual-tools', icon: <VisualToolsIcon />, label: 'Visual Tools' },
+    { href: '/dashboard/community', icon: <CommunityIcon />, label: 'Community' },
 ];
 
 function DashboardLayoutContent({ children }: { children: React.ReactNode }) {
@@ -151,4 +151,3 @@ export default function DashboardLayout({
     <DashboardLayoutContent>{children}</DashboardLayoutContent>
   );
 }
-
