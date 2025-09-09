83b1fe65b989b1bc096b098be87432614270f0c0
Chris White <comresusa@gmail.com>
2025-09-09 00:01:01 +0000

    for the calander page only were going to chage it up heres page.tsx for

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,13 +72,20 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 0.5s ease-in-out",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
 };
 export default config;
+
+    
