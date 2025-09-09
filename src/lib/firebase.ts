0cecd6380c5a9431fdfb6f5a374ee49d2a948824
Chris White <comresusa@gmail.com>
2025-09-09 18:30:50 +0000

    same error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "learnwise-r6us0",
-  "appId": "1:224206262515:web:d5960f2bc62f97be5ba786",
-  "storageBucket": "learnwise-r6us0.appspot.com",
-  "apiKey": "AIzaSyC8nFez_Ye_qT0kVxEYK7uhGB6oQRbRfU0",
-  "authDomain": "learnwise-r6us0.firebaseapp.com",
-  "messagingSenderId": "224206262515"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
