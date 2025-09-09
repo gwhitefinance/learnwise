3379c6a5f8a67282f95cdeb3cc15886c90ab314c
Chris White <comresusa@gmail.com>
2025-09-09 18:41:10 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyC8nFez_Ye_qT0kVxEYK7uhGB6oQRbRfU0",
   authDomain: "learnwise-r6us0.firebaseapp.com",
   projectId: "learnwise-r6us0",
   storageBucket: "learnwise-r6us0.appspot.com",
   messagingSenderId: "224206262515",
-  appId: "1:224206262515:web:d5960f2bc62f97be5ba786",
+  appId: "1:224206262515:web:d5960f2bc62f97be5ba786"
 };
 
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
