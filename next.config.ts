1f01990c945aeafed9001c9fa59c07813162a2ba
Chris White <comresusa@gmail.com>
2025-09-11 23:54:08 +0000

    disable dev tools
    Disable Dev Tools for this project
    To disable this U

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  devIndicators: {
+    buildActivity: false
+  },
   transpilePackages: ['@splinetool/react-spline'],
   images: {
     remotePatterns: [
