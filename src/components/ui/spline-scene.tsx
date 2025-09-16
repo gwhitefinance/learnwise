60a6b924331bece533218d26a3e18781c6140660
Chris White <comresusa@gmail.com>
2025-09-16 17:33:27 +0000

    Try fixing this error: `Runtime Error: Error: Data read, but end of buff

--- src/components/ui/spline-scene.tsx
+++ src/components/ui/spline-scene.tsx
@@ -1,8 +1,7 @@
 
 "use client"
 
-import { Suspense, lazy } from "react"
-const Spline = lazy(() => import("@splinetool/react-spline"))
+import Spline from "@splinetool/react-spline";
 
 interface SplineSceneProps {
   scene: string
@@ -11,16 +10,6 @@ interface SplineSceneProps {
 
 export default function SplineScene({ scene, className }: SplineSceneProps) {
   return (
-    <Suspense
-      fallback={
-        <div className="w-full h-full flex items-center justify-center bg-black/5 rounded-lg">
-          <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary"></div>
-        </div>
-      }
-    >
-      <Spline scene={scene} className={className} />
-    </Suspense>
+    <Spline scene={scene} className={className} />
   )
 }
-
-    
