59a37443327ce77e5dd017e7296b199a9759312a
Chris White <comresusa@gmail.com>
2025-09-16 18:03:36 +0000

    Try fixing this error: `Console Error: <u> is an async Client Component.

--- src/components/ui/spline-scene.tsx
+++ src/components/ui/spline-scene.tsx
@@ -1,20 +1,17 @@
 'use client'
 
 import Spline from '@splinetool/react-spline/next';
-import { Application } from '@splinetool/runtime';
 
 interface SplineSceneProps {
   scene: string;
   className?: string;
-  onLoad?: (spline: Application) => void;
 }
 
-export default function SplineScene({ scene, className, onLoad }: SplineSceneProps) {
+export default function SplineScene({ scene, className }: SplineSceneProps) {
   return (
     <Spline
         scene={scene}
         className={className}
-        onLoad={onLoad}
     />
   )
 }
