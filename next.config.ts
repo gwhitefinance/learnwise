0641a36df26e29f36aa07073b867cb34b2c66b8e
Chris White <comresusa@gmail.com>
2025-09-18 02:34:24 +0000

    still doesnt load

--- next.config.ts
+++ next.config.ts
@@ -89,12 +89,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('handlebars');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
