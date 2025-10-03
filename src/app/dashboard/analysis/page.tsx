fb7a71a7d596022e51ffea5641fca193554b79e5
Chris White <comresusa@gmail.com>
2025-10-03 18:06:16 +0000

    at n3 (/home/user/studio/node_modules/next/dist/compiled/next-server/app

--- src/app/dashboard/analysis/page.tsx
+++ src/app/dashboard/analysis/page.tsx
@@ -4,6 +4,8 @@ import dynamic from 'next/dynamic';
 import Loading from './loading';
 import { Suspense } from 'react';
 
+export const dynamic = "force-dynamic";
+
 const AnalysisClientPage = dynamic(() => import('./AnalysisClientPage'), {
     ssr: false,
     loading: () => <Loading />,
