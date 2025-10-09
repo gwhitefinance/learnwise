c23530b1876aae8d22ce60ae99a1c82613ff4ba6
Chris White <comresusa@gmail.com>
2025-10-09 02:32:49 +0000

    it still doesnt have the title for the modules in that blank space

--- src/app/dashboard/courses/[courseId]/ClientCoursePage.tsx
+++ src/app/dashboard/courses/[courseId]/ClientCoursePage.tsx
@@ -277,15 +277,10 @@ export default function ClientCoursePage() {
     return (
         <div className="space-y-6">
             <Skeleton className="h-10 w-40" />
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                <div className="md:col-span-2 space-y-4">
-                    <Skeleton className="h-12 w-3/4" />
-                    <Skeleton className="h-6 w-1/2" />
-                    <Skeleton className="h-24 w-full" />
-                </div>
-                <div className="space-y-4">
-                    <Skeleton className="aspect-[3/2] w-full" />
-                </div>
+            <div className="space-y-4">
+                <Skeleton className="h-12 w-3/4" />
+                <Skeleton className="h-6 w-1/2" />
+                <Skeleton className="h-24 w-full" />
             </div>
             <div className="space-y-4">
                 <Skeleton className="h-8 w-48" />
