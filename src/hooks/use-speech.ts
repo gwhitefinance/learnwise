e537605b20b163e857084f570ec849fe575b04d4
Chris White <comresusa@gmail.com>
2025-10-16 17:58:35 +0000

    Try fixing this error: `Runtime ReferenceError: useRef is not defined. E

--- src/hooks/use-speech.ts
+++ src/hooks/use-speech.ts
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import { useToast } from './use-toast';
 
 export const useSpeech = () => {
