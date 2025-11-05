3d317833aa2b6bc4137ab7f02912241bc3db9d0f
Chris White <comresusa@gmail.com>
2025-11-05 00:48:16 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://images.

--- next.config.js
+++ next.config.js
@@ -106,6 +106,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
