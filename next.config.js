ba9a55908a26c3c6ef54107541665e5b9ca97e40
Chris White <comresusa@gmail.com>
2025-11-05 15:22:02 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://lh3.goo

--- next.config.js
+++ next.config.js
@@ -112,6 +112,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
