b083993dc8fce57c83aabb544ab4670e3728fda3
Chris White <comresusa@gmail.com>
2025-11-10 06:35:06 +0000

    use this for the logo
    https://i.postimg.cc/gcsGkWRz/Tutor-1-removebg-p

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const Logo = ({ className }: { className?: string }) => (
   <Image
-    src="https://i.postimg.cc/wjNyZZmQ/Tutor-removebg-preview.png"
+    src="https://i.postimg.cc/gcsGkWRz/Tutor-1-removebg-preview.png"
     alt="Tutor Taz Logo"
     width={80}
     height={80}
