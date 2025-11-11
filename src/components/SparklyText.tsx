62b59846eb34eb4cfb67f00bc59428bb84d59a24
Chris White <comresusa@gmail.com>
2025-11-11 01:29:43 +0000

    get rid of all the emojis around effortlessly

--- src/components/SparklyText.tsx
+++ src/components/SparklyText.tsx
@@ -12,10 +12,6 @@ const SparklyText = ({ text }: { text: string }) => {
             <span ref={textRef} className="sparkly-text">
                 {text}
             </span>
-             <div className="star" style={{ top: '-40px', left: '-30px', animationDelay: '0s' }}>✨</div>
-            <div className="star" style={{ top: '-50px', right: '-40px', animationDelay: '0.5s' }}>⭐</div>
-            <div className="star" style={{ bottom: '-40px', left: '20px', animationDelay: '1s' }}>✨</div>
-            <div className="star" style={{ bottom: '-30px', right: '30px', animationDelay: '0.3s' }}>⭐</div>
         </div>
     );
 };
