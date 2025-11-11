6fe30498397fa1167829cb061ea6cd45f5473825
Chris White <comresusa@gmail.com>
2025-11-11 01:32:00 +0000

    i like the green hue and cartoon character font i js want a fast version

--- src/components/SparklyText.tsx
+++ src/components/SparklyText.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const SparklyText = ({ text }: { text: string }) => {
     return (
-        <span className="text-blue-400">
+        <span className="text-green-500 font-bold [text-shadow:0_0_10px_rgba(34,197,94,0.5)]">
             {text}
         </span>
     );
