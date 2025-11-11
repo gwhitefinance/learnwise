750e8fbb3d5d30e9ea88c3ec1b605694847177a1
Chris White <comresusa@gmail.com>
2025-11-11 02:04:10 +0000

    no the same font as in this code <!DOCTYPE html> <html lang="en"> <head

--- src/components/SparklyText.tsx
+++ src/components/SparklyText.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const SparklyText = ({ text }: { text: string }) => {
     return (
-        <span className="text-green-500 font-bubble">
+        <span className="text-green-500" style={{ fontFamily: "'Comic Sans MS', 'Arial Black', sans-serif" }}>
             {text}
         </span>
     );
