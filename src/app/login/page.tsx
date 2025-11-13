450ab6a0a2854bcb352caac07062a4c149663350
Chris White <comresusa@gmail.com>
2025-11-13 17:56:40 +0000

    design the background of the login page to be a winter wonderland snow f

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,6 +15,74 @@ import { cn } from "@/lib/utils"
 import { motion } from 'framer-motion';
 import { collection, query, where, getDocs, updateDoc, arrayUnion, writeBatch, setDoc, serverTimestamp, doc } from 'firebase/firestore';
 
+const Snowflake = ({ style }: { style: React.CSSProperties }) => (
+    <motion.div
+        className="absolute rounded-full bg-white"
+        style={style}
+        animate={{ y: '100vh', opacity: [1, 1, 0] }}
+        transition={{
+            duration: Math.random() * 10 + 10,
+            repeat: Infinity,
+            ease: 'linear',
+            delay: Math.random() * 5,
+        }}
+    />
+);
+
+const WinterWonderland = () => (
+    <div className="absolute inset-0 overflow-hidden -z-10">
+        {/* Sky */}
+        <div className="absolute inset-0 bg-gradient-to-b from-gray-800 to-gray-900" />
+
+        {/* Snowflakes */}
+        {[...Array(100)].map((_, i) => {
+            const size = Math.random() * 3 + 1;
+            return (
+                <Snowflake
+                    key={i}
+                    style={{
+                        left: `${Math.random() * 100}%`,
+                        top: '-10px',
+                        width: size,
+                        height: size,
+                        opacity: Math.random() * 0.5 + 0.3,
+                    }}
+                />
+            );
+        })}
+
+        {/* Snowy ground */}
+        <div className="absolute bottom-0 left-0 w-full h-1/4 bg-white" />
+
+        {/* Christmas Tree */}
+        <div className="absolute bottom-[23%] left-[10%] w-0 h-0" style={{ transform: 'scale(0.8)' }}>
+            <div className="relative">
+                <div className="absolute -top-16 left-1/2 -translate-x-1/2 text-yellow-400 text-3xl">★</div>
+                <div className="w-0 h-0 border-l-[60px] border-l-transparent border-r-[60px] border-r-transparent border-b-[100px] border-b-green-700 relative z-10" />
+                <div className="absolute top-10 left-1/2 -translate-x-1/2 w-0 h-0 border-l-[50px] border-l-transparent border-r-[50px] border-r-transparent border-b-[80px] border-b-green-600 z-20" />
+                <div className="absolute top-16 left-1/2 -translate-x-1/2 w-0 h-0 border-l-[40px] border-l-transparent border-r-[40px] border-r-transparent border-b-[60px] border-b-green-500 z-30" />
+                <div className="absolute bottom-[-10px] left-1/2 -translate-x-1/2 w-8 h-10 bg-yellow-900 z-0" />
+                {/* Ornaments */}
+                <div className="absolute top-8 left-10 w-4 h-4 bg-red-500 rounded-full z-40" />
+                <div className="absolute top-16 left-3 w-4 h-4 bg-blue-500 rounded-full z-40" />
+                <div className="absolute top-20 right-5 w-4 h-4 bg-yellow-500 rounded-full z-40" />
+            </div>
+        </div>
+
+        {/* Gifts */}
+         <div className="absolute bottom-[24%] right-[12%] flex items-end gap-2">
+            <div className="w-16 h-16 bg-red-600 relative">
+                <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 h-4 bg-yellow-400" />
+                <div className="absolute inset-y-0 left-1/2 -translate-x-1/2 w-4 bg-yellow-400" />
+            </div>
+            <div className="w-20 h-20 bg-blue-600 relative">
+                <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 h-5 bg-green-400" />
+                <div className="absolute inset-y-0 left-1/2 -translate-x-1/2 w-5 bg-green-400" />
+            </div>
+        </div>
+    </div>
+);
+
 
 const GoogleIcon = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"></path><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"></path><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"></path><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"></path><path d="M1 1h22v22H1z" fill="none"></path></svg>;
 
@@ -121,10 +189,11 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background p-4 relative overflow-hidden">
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 p-4 relative overflow-hidden">
+        <WinterWonderland />
        <Link href="/" className="absolute top-4 left-4 z-20">
             <Button variant="ghost" size="icon" className="bg-black/10 hover:bg-black/20 text-gray-800 rounded-full h-10 w-10">
-              <X className="h-5 w-5" />
+              <X className="h-5 w-5 text-white" />
             </Button>
         </Link>
 
