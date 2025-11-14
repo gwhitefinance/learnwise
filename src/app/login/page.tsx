8e430086dc05dfa62aae450d2782163d9fe82e49
Chris White <comresusa@gmail.com>
2025-11-14 03:44:14 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 
+
 'use client';
 
 import * as React from "react";
@@ -18,6 +19,52 @@ import { collection, query, where, getDocs, updateDoc, arrayUnion, writeBatch, s
 
 
 const GoogleIcon = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"></path><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"></path><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"></path><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"></path><path d="M1 1h22v22H1z" fill="none"></path></svg>;
+
+const ChristmasLights = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <svg
+            className="absolute top-0 left-0 w-full h-auto"
+            viewBox="0 0 1440 80"
+            preserveAspectRatio="none"
+        >
+            <path
+                d="M0,40 C200,80 400,20 600,40 C800,60 1000,20 1200,40 C1400,60 1440,30 1440,30 L1440,0 L0,0 Z"
+                stroke="#4A5568"
+                strokeWidth="2"
+                fill="none"
+            />
+            <g>
+                {[...Array(20)].map((_, i) => (
+                    <motion.circle
+                        key={i}
+                        cx={70 * (i + 1)}
+                        cy={40 + 20 * Math.sin((i * Math.PI) / 5)}
+                        r="8"
+                        fill={['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#FFFF33'][i % 5]}
+                    >
+                        <animate
+                            attributeName="opacity"
+                            values="0.5;1;0.5"
+                            dur="2s"
+                            begin={`${i * 0.2}s`}
+                            repeatCount="indefinite"
+                        />
+                    </motion.circle>
+                ))}
+            </g>
+        </svg>
+    );
+};
+
 
 export default function LoginPage() {
   const router = useRouter()
@@ -123,37 +170,7 @@ export default function LoginPage() {
 
   return (
     <div className="flex items-center justify-center h-screen w-screen relative overflow-hidden">
-        <svg
-            className="absolute top-0 left-0 w-full h-auto"
-            viewBox="0 0 1440 80"
-            preserveAspectRatio="none"
-        >
-            <path
-                d="M0,40 C200,80 400,20 600,40 C800,60 1000,20 1200,40 C1400,60 1440,30 1440,30 L1440,0 L0,0 Z"
-                stroke="#4A5568"
-                strokeWidth="2"
-                fill="none"
-            />
-            <g>
-                {[...Array(20)].map((_, i) => (
-                    <motion.circle
-                        key={i}
-                        cx={70 * (i + 1)}
-                        cy={40 + 20 * Math.sin((i * Math.PI) / 5)}
-                        r="8"
-                        fill={['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#FFFF33'][i % 5]}
-                    >
-                        <animate
-                            attributeName="opacity"
-                            values="0.5;1;0.5"
-                            dur="2s"
-                            begin={`${i * 0.2}s`}
-                            repeatCount="indefinite"
-                        />
-                    </motion.circle>
-                ))}
-            </g>
-        </svg>
+        <ChristmasLights />
 
         <Link href="/" className="absolute top-4 left-4 z-20">
             <Button variant="ghost" size="icon" className="bg-black/10 hover:bg-black/20 text-white rounded-full h-10 w-10">
@@ -167,10 +184,10 @@ export default function LoginPage() {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5, ease: 'easeOut' }}
       >
-        <div className="absolute -top-4 -left-4 transform -rotate-12 w-14 h-14">
+        <div className="absolute -top-4 -left-4 transform -rotate-12" style={{width: '25px', height: '25px'}}>
             <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 512 512"><path d="M473.187 0H193.989c-12.849 0-23.266 10.417-23.266 23.266v108.578c0 12.849 10.417 23.266 23.266 23.266h279.194c12.849 0 23.266-10.417 23.266-23.266V23.266C496.453 10.417 486.036 0 473.187 0" style={{fill:'#dedee0'}}/><path d="M442.165 155.111H201.471V248l-53.946.04c-72.773 0-131.977 59.205-131.977 131.978C15.547 452.793 74.75 512 147.523 512h.008l185.918-.059q.07-.002.14-.003c72.712-.076 131.843-59.25 131.843-131.977v-224.85z" style={{fill:'#d8143a'}}/><path d="M248.002 155.111h85.586V0H193.989c-12.849 0-23.266 10.417-23.266 23.266v108.578c0 12.849 10.417 23.266 23.266 23.266h54.013z" style={{fill:'#d6d5d8'}}/><path d="M147.523 512h.008l185.918-.059q.07-.002.14-.003V155.111H201.471V248l-53.946.04c-72.773 0-131.977 59.205-131.977 131.978C15.547 452.793 74.75 512 147.523 512" style={{fill:'#b8002b'}}/></svg>
         </div>
-        <div className="absolute -top-4 -right-4 transform rotate-12 scale-x-[-1] w-14 h-14">
+        <div className="absolute -top-4 -right-4 transform rotate-12 scale-x-[-1]" style={{width: '25px', height: '25px'}}>
              <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 512 512"><path d="M473.187 0H193.989c-12.849 0-23.266 10.417-23.266 23.266v108.578c0 12.849 10.417 23.266 23.266 23.266h279.194c12.849 0 23.266-10.417 23.266-23.266V23.266C496.453 10.417 486.036 0 473.187 0" style={{fill:'#dedee0'}}/><path d="M442.165 155.111H201.471V248l-53.946.04c-72.773 0-131.977 59.205-131.977 131.978C15.547 452.793 74.75 512 147.523 512h.008l185.918-.059q.07-.002.14-.003c72.712-.076 131.843-59.25 131.843-131.977v-224.85z" style={{fill:'#d8143a'}}/><path d="M248.002 155.111h85.586V0H193.989c-12.849 0-23.266 10.417-23.266 23.266v108.578c0 12.849 10.417 23.266 23.266 23.266h54.013z" style={{fill:'#d6d5d8'}}/><path d="M147.523 512h.008l185.918-.059q.07-.002.14-.003V155.111H201.471V248l-53.946.04c-72.773 0-131.977 59.205-131.977 131.978C15.547 452.793 74.75 512 147.523 512" style={{fill:'#b8002b'}}/></svg>
         </div>
         <div className="text-center">
